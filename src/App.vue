b8912d642836422db583efffb620b1e5f271734e
Elouan Le Bars <elouan@coopiteasy.be>
2020-06-28 18:15:00 +0200

    [ADD] Results component

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,20 @@
       <Scenario title="Scenario A"></Scenario>
       <Scenario title="Scenario B"></Scenario>
       <Scenario title="Scenario C"></Scenario>
+      <Results/>
     </div>
   </div>
 </template>
 
 <script>
 import Scenario from "./components/Scenario.vue";
+import Results from "./components/Results.vue";
 
 export default {
   name: "App",
   components: {
     Scenario,
+    Results
   },
 };
 </script>
---
+++ src/components/Results.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="results">
+      <div class="results-header">
+          Résultats
+          <button class="export-btn">Exporter</button>
+      </div>
+      <div class="results-content">
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.results-header {
+  font-weight: bold;
+  font-size: 30px;
+  line-height: 35px;
+  color: #3B3B3B;
+  padding-top: 55px;
+  padding-left: 40px;
+  /* ! This padding-right should be removed */
+  padding-right: 160px;
+}
+.export-btn {
+  border: none;
+  color: black;
+  background: rgba(58, 104, 224, 0.5);
+  border-radius: 6px;
+  padding: 5px 10px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+}
+.export-btn:hover {
+  background: rgba(0, 71, 255, 0.58);
+}
+</style>
