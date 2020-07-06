b36c7a7e19f41da46afe4f75176b7815a6aa8371
Matt Glorion <matt@glorion.fr>
2020-07-06 23:06:57 +0200

    [UPD] rename impact spheres

--- src/options/detailled_results_text.js
+++ src/options/detailled_results_text.js
@@ -1,12 +1,12 @@
 export const detailled_results_text = {
-  sante_humaine:
+  HUMAN_HEALTH:
     `L’impact sur la santé humaine est mesuré en DALY (Disability Adjusted Life Years)
     et exprimé en pourcentage par rapport au scénario ayant le plus d’impact.
     Cette méthode d’évaluation consiste à déterminer l’impact en année de vies de la réunion sur une vie humaine.
     Elle prend en compte la toxicité de la réunion sur la santé humaine, les effets respiratoires,
     les rayonnements ionisants et la diminution de la couche.`
   ,
-  qualite_ecosysteme:
+  ECOSYSTEM_QUALITY:
     `L’impact sur la qualité de l’écosystème est mesuré en PDF*m^2*année et exprimé en
     pourcentage par rapport au scénario ayant le plus d’impact. Le PDF (Potentially Disappeared Fraction of species)
     correspond à la fraction des espèces qui a une forte probabilité de ne pas se trouver dans une région en raison
@@ -14,12 +14,12 @@ export const detailled_results_text = {
     l'oxydation photochimique, l’écotoxicité aquatique et terrestre, l’acidification et l’eutrophisation des eaux
     et des terres et l’occupation des terres.`
   ,
-  changement_climatique:
+  CLIMATE_CHANGE:
     `L’impact sur le changement climatique est mesuré en Kg eqCO2 et exprimé en pourcentage par
     rapport au scénario ayant le plus d’impact. L'équivalent CO₂ étant, pour un gaz à effet de serre,
     la quantité de dioxyde de carbone qui aurait la même capacité à retenir le rayonnement solaire.`
   ,
-  ressources:
+  RESOURCES:
     `L’impact sur les ressources considère l’utilisation d’énergies non renouvelables et
     l'extraction de minéraux. Il est mesuré en MJ primaire et exprimé en pourcentage par rapport au scénario
     ayant le plus d’impact. Cette quantité d’énergie primaire correspond à la quantité d’énergie contenues dans
